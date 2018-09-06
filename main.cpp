da444cde27708eb52fafcf61c1ee074b9f6afc0f
DrSianArmstrong <39267412+DrSianArmstrong@users.noreply.github.com>
2018-09-06 16:48:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /*
-This is me trying to alter the initial Hello World program to have a timer in it as demoed
-by I'm not entirely sure who, but the program was called Interrupt_TImer_LED
+This is me trying to alter the initial Hello World program to have a timer in it 
 */
 #include "MicroBit.h"
 //#include "mbed.h"
@@ -16,11 +15,6 @@ Ticker      interrrrupt;
 //Prototype
 //------------------------------------------------------------------//
 void intTimer( void );                  /* Interrupt function       */
-void timer( unsigned long timer_set );
-
-//Globle
-//------------------------------------------------------------------//
-volatile unsigned long  cnt_timer;      /* Used by timer function   */
 
 int main() {
  uBit.init();
@@ -36,8 +30,6 @@ int main() {
 }
 
 
-
-
 //Interrupt Timer
 //------------------------------------------------------------------//
 void intTimer( void )
@@ -45,14 +37,7 @@ void intTimer( void )
     uBit.io.P0.setDigitalValue(!(uBit.io.P0.getDigitalValue()));
 }
 
-//Timer function
-//------------------------------------------------------------------//
-void timer( unsigned long timer_set )
-{
-    cnt_timer = 0;
-    while( cnt_timer < timer_set );
-}
+
 
 //------------------------------------------------------------------//
 // End of file
-//------------------------------------------------------------------//
