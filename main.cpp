2fc0d4342cf43a985de6f97f34e551e44dd7ba8e
DrSianArmstrong <39267412+DrSianArmstrong@users.noreply.github.com>
2018-09-06 15:35:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ MicroBit uBit;
 
 //Constructor
 //------------------------------------------------------------------//
-Ticker      interrput;
+Ticker      interrrrupt;
 
 
 //Prototype
 //------------------------------------------------------------------//
-void intTimer( void );                  /* Interrupt fanction       */
+void intTimer( void );                  /* Interrupt function       */
 void timer( unsigned long timer_set );
 
 //Globle
@@ -24,15 +24,12 @@ volatile unsigned long  cnt_timer;      /* Used by timer function   */
 
 int main() {
  uBit.init();
- uBit.display.scroll("Really Pissed off");
+ uBit.display.scroll("So frustrating");
     /* Initialize MCU functions */
-    interrput.attach(&intTimer, 0.001);
+    interrrrupt.attach(&intTimer, 0.001);
  
     while(1) {
- //       uBit.io.P0.setDigitalValue(1);
- //       timer( 1000 );
- //       uBit.io.P0.setDigitalValue(0);
- //       timer( 1000 );
+
     }
     
  release_fiber();
@@ -46,10 +43,9 @@ int main() {
 void intTimer( void )
 {
     uBit.io.P0.setDigitalValue(!(uBit.io.P0.getDigitalValue()));
-//    cnt_timer++;
 }
 
-//Timer fanction
+//Timer function
 //------------------------------------------------------------------//
 void timer( unsigned long timer_set )
 {
